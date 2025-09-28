0ec3c292d22cae53160c2b167e794b146c01494a
XIE Zhengyang <114273988+SORMaker@users.noreply.github.com>
2025-09-28 14:38:07 +0800

    feat: add week4 project (#2)

---
+++ week4/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "log.h"
+
+using namespace::std;
+
+int main(){
+
+    // 12
+    // int x = 5;
+    // bool comparisonResult = x == 5;
+
+    // const char* ptr = "Hello";
+    // if (ptr)
+    // {
+    //     Log(ptr);
+    // }
+
+    // const char* ptr = "Hello"; 
+
+    // if(ptr == "Hello")
+    // {
+    //     Log("Ptr is Hello");
+    // }
+    // else if (ptr)
+    // {
+    //     Log(ptr);
+    // }
+    // else
+    // {
+    //     Log("Ptr is null!");
+    // }
+ 
+    // 14    
+    // for (int i = 0; i < 5; i++){
+    //     Log("Hello World!");
+    // }
+    // int i = 0;
+    // bool condition = true;
+    // for (; condition; ){
+    //     Log("Hello World!");
+    //     i++;
+    //     if(!(i < 5)){
+    //         condition = false;
+    //     }
+    // }
+    
+    // int i = 0;
+    // while (i < 5)
+    // {
+    //     Log("Hello World!");
+    //     i++;
+    // }
+    
+
+    // do
+    // {
+    //     Log("Hello World!");
+    //     i--;
+    // } while (i > 0);
+    
+    // 15
+    // for (int i = 0; i < 5; i++){
+    //     // if (i % 2 == 0)
+    //     // if (i > 2)
+    //     if ((i + 1) % 2 == 0)
+    //     {
+    //         // continue;
+    //         // break;
+    //         return 0;
+    //     }
+        
+    //     Log("Hello World!");
+    //     cout << i << endl;
+    // }
+
+    // if (5 > 8)
+    // {
+    //     return 0;
+    // }
+    
+
+    return 0;
+}
